0f0eb1de8d2e4e58d1f009f11d53042095f50c90
Dagmawi <dagmawinebeyu3@gmail.com>
2022-04-04 19:25:10 +0300

    Add modules, factory functions and scores + round counters

--- script.js
+++ script.js
@@ -1,84 +1,152 @@
 
-let playerOne = 'X';
-let playerTwo = 'O';
 
-const cells = document.querySelectorAll('.cell');
-const roundCounter = document.getElementById('roundCounter');
-const p1score = document.getElementById('X');
-const p2score = document.getElementById('O');
 
-let gameBoard;
-let player = playerOne;
+const player = (name, sign) => {
+    let score = 0;
 
-const winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+    const getName = () => name;
+    const getSign = () => sign;
+    const incrementScore = () => score++;
+    const getScore = () => score;
+    const resetScore = () => score = 0;
 
-startGame()
-function startGame() {
-    cells.forEach(cell => {
-        cell.innerText = '';
-        cell.style.backgroundColor = "transparent";
-        gameBoard = new Array(9);
-        cell.addEventListener('click', render, false)
-    }) 
-
+    return {getName, getSign, incrementScore, getScore, resetScore}
 }
 
-function render(event) {
-    event.target.innerText = player;
-    gameBoard[event.target.id] = player;
-    checkWin(player)
-    switchTurns()
-}
 
-function checkWin(player) {
- const moves = gameBoard.reduce((a, e, i) => {
-    return (e === player) ? a.concat(i) : a
- }, []);
+const gameBoard = (() => {
+    const cells = document.querySelectorAll('.cell');
+    const roundCounter = document.getElementById('roundCounter');
+    const p1score = document.getElementById('X');
+    const p2score = document.getElementById('O');
 
- winCombos.forEach((combo) => {
-    if(combo.every(position => moves.indexOf(position) != -1)) {
-        let roundWinner = {player: player};
+    const playerOne = player('tempName', 'X')
+    const playerTwo = player('tempName2', 'O')
 
-        for(let i = 0; i < combo.length; i++) {
-            document.getElementById(`${combo[i]}`).style.backgroundColor = 
-            player == playerOne ? 'lightblue' : 'red';         
-        }
-        cells.forEach(cell => cell.removeEventListener('click', render, false))
-        setTimeout(incrementRound, 3000)
-        setTimeout(startGame, 3000)
+    let currentPlayer = playerOne;
+    let gameboard;
+    let currentIndex;
+    let currentSign;
+    let winScore;
+    let rounds = 1;
+    const winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-        // updateScore()
-        // checkRound()
-        // return roundWinner
+    const startGame = () => {
+        gameboard = new Array(9);
+        displayController.clearBoard()
+        let endRound = false;
+
+        cells.forEach(cell => {
+            cell.addEventListener('click', render, false);
+
+        }) 
+
+        if(endRound == true) {
+            displayController.resetBoardScore()
+            displayController.resetBoardRound(roundCounter)
+            rounds = 1
+            playerOne.resetScore()
+            playerTwo.resetScore()
+        }   
     }
- })
-}
+
+    function render(event) {
+        currentIndex = event.target.id;
+        currentSign = currentPlayer.getSign(); // created these vars for use in displaycontroller b/c it couldn't access currentPlayer.getSign()
+
+        gameboard[currentIndex] = currentSign;
+        displayController.fillCell(currentIndex, currentSign)
+        checkWin(currentPlayer)
+        switchTurns()
+    }
+    
+    function switchTurns() {
+        if(currentPlayer == playerOne) {
+            return currentPlayer = playerTwo;
+        }
+        else {
+            return currentPlayer = playerOne;
+        }
+    }
+    
+    function checkWin(currentPlayer) {
+     const moves = gameboard.reduce((a, e, i) => {
+        return (e === currentSign) ? a.concat(i) : a
+     }, []);
+    
+     winCombos.forEach((combo) => {
+        if(combo.every(position => moves.indexOf(position) != -1)) {
+    
+            for(let i = 0; i < combo.length; i++) {
+                document.getElementById(`${combo[i]}`).style.backgroundColor = 
+                currentPlayer == playerOne ? 'lightblue' : 'red';         
+            }
+            cells.forEach(cell => cell.removeEventListener('click', render, false)) 
+            
+            currentPlayer.incrementScore()
+            winScore = currentPlayer.getScore()
+            displayController.renderScore(winScore, currentSign)
+            
+
+            checkRound(rounds)
+            displayController.incrementRound(roundCounter)
+            rounds++
+            setTimeout(startGame, 1500)
+            // return roundWinner
+        }
+     })
+    }
+
+    function checkRound(rounds) {
+        if(rounds == 3) alert ('gameover')
+        endRound = true;
+    }
+
+    return {startGame, currentPlayer};
+})();
+
+const displayController = (() => {
+    const cells = document.querySelectorAll('.cell');
+    const clearBoard = () => {
+        cells.forEach(cell => {
+            cell.innerText = '';
+            cell.style.backgroundColor = "transparent";
+            // cell.style.cssText = "animation: fade-out 0.3s ease forwards;";
+            // cell.addEventListener("animationend", () => {
+            //         square.innerHTML = "";
+        }) 
+    }
+
+    const fillCell = (currentIndex, currentSign) => { //notice here u don't need to put gameBoard.
+        document.getElementById(currentIndex).innerText = currentSign;
+    }
+    const renderScore = (winScore, currentSign) => document.getElementById(currentSign).innerText = ' ' + winScore;
+    const incrementRound = (roundCounter) => roundCounter.innerText = parseInt(roundCounter.innerText) + 1;
+    const resetBoardScore = () => {
+        document.getElementById('X').innerText = 0;
+        document.getElementById('O').innerText = 0;      
+    }
+    const resetBoardRound = (roundCounter) => roundCounter.innerText = 0
+    return {clearBoard, fillCell, renderScore, incrementRound, resetBoardScore, resetBoardRound}
+})();
+
+gameBoard.startGame()
 
 // function updateScore(roundWinner) {
 //     document.getElementById('roundWinner.player').innerText = 
 // }
 
-function incrementRound() {
-    roundCounter.innerText = parseInt(roundCounter.innerText) + 1;
-}
 
-function switchTurns() {
-    if(player == playerOne) {
-        return player = playerTwo;
-    }
-    else {
-        return player = playerOne;
-    }
-}
+
 
 
 
